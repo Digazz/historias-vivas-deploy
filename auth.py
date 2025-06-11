ba5c9760b730ed8bc379cae3f4aae11798fdbf08
DLS <82974413+Digazz@users.noreply.github.com>
2025-06-11 16:39:12 -0300

    Delete auth.py

--- auth.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-import hmac
-
-def hash_password(senha):
-    return hashlib.sha256(senha.encode()).hexdigest()
-
-def verify_password(senha, senha_hash):
-    return hmac.compare_digest(hash_password(senha), senha_hash)
